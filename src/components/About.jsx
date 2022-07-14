230c40f8997dc0e7d354913b391d6b6688075bb6
AndreyPonomarou <79516625+AndreyPonomarou@users.noreply.github.com>
2022-07-14 19:26:13 +0300

    -newtext

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,9 +7,9 @@ const About = () => {
             <div className="container py-5 my-5">
                 <div className="row">
                     <div className="col-md-6">
-                        <h1 className="text-primary fw-bold mb-4">About Us</h1>
+                        <h1 className="text-primary fw-bold mb-4">О нас</h1>
                         <p className="lead mb-4">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo molestiae earum rem doloremque, nihil delectus ullam error consectetur? Dicta, non exercitationem in consectetur totam dolorum at voluptate laudantium aliquam, officiis perspiciatis molestias reiciendis consequuntur ullam perferendis velit blanditiis distinctio assumenda a maxime reprehenderit atque. Nam eius rerum distinctio, a illo earum, optio molestias nostrum maxime quibusdam delectus, adipisci impedit? Nam corporis reiciendis minus quod eaque, laborum veritatis voluptatibus id maiores tempore accusantium recusandae perspiciatis, officia cum ad maxime fuga repellendus a magni consequatur. Unde adipisci hic provident est sint corporis, dolorem esse autem soluta molestiae optio quisquam eligendi obcaecati minima?
+                        Кстати, некоторые особенности внутренней политики, превозмогая сложившуюся непростую экономическую ситуацию, объявлены нарушающими общечеловеческие нормы этики и морали. Высокий уровень вовлечения представителей целевой аудитории является четким доказательством простого факта: сложившаяся структура организации способствует повышению качества своевременного выполнения сверхзадачи. Современные технологии достигли такого уровня, что разбавленное изрядной долей эмпатии, рациональное мышление обеспечивает актуальность как самодостаточных, так и внешне зависимых концептуальных решений.
                         </p>
                         <NavLink to="/contact" className="btn btn-outline-primary px-3">Свяжитесь с нами</NavLink>
                     </div>
